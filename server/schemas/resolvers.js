91b150f7bca0da82b2966688c6373ae8a959c754
KyleM021199 <kylem021199@gmail.com>
2023-08-01 13:17:04 -0500

    setup resolvers and typeDefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const  { User } = require('../models/User');
+
+const resolvers = {
+    Query: {
+
+    },
+
+    Mutation: {
+
+    }
+}
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,12 @@
+const typeDefs = `
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+}
+
+
+`
+
+module.exports = typeDefs;
