7fe208aaccb1fef35fdecedd08947e4b310ee3be
KyleM021199 <kylem021199@gmail.com>
2023-08-02 00:53:45 -0500

    more setup pon resolvers and typeDefs and made client files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,28 @@
-const  { User } = require('../models/User');
+const  { User, Book} = require('../models/User');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-
+        user: async () =>{
+            return User.find({});
+        },
     },
 
     Mutation: {
+        login: async(parent, args) =>{
 
-    }
+        },
+        addUser: async(parent, args) =>{
+            const user = await Book.create(args);
+            return user;
+        },
+        saveBook: async(parent, args) =>{
+
+        },
+        removeBook:async(parent, args) =>{
+            
+        }
+
+    },
 }
+module.exports = resolvers;
